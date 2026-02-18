6a3944b8508d0b48cbfccc36125c6a8619854132
Renato Groffe <renato.groffe@yahoo.com.br>
2026-02-18 11:15:14 -0300

    Commit inicial

---
+++ src/ConsoleAppIReadOnlySet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 11 | IReadOnlySet<T> + Desserializacao System.Text.Json *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+IReadOnlySet<string> campeoesMundiais = new HashSet<string>()
+{
+    "Brasil",
+    "Italia",
+    "Alemanha",
+    "Argentina",
+    "Uruguai",
+    "Franca",
+    "Inglaterra",
+    "Espanha"
+};
+var jsonData = JsonSerializer.Serialize(campeoesMundiais);
+
+Console.WriteLine();
+Console.WriteLine($"Resultado da serializacao JSON - Referencia *{nameof(jsonData)}*: ");
+Console.WriteLine(jsonData);
+
+Console.WriteLine();
+Console.WriteLine("Desserializando string JSON...");
+var campeoesMundiaisDeserialized =
+    JsonSerializer.Deserialize<IReadOnlySet<string>>(jsonData)!;
+
+Console.WriteLine();
+Console.WriteLine($"Resultado da serializacao JSON - Referencia *{nameof(campeoesMundiaisDeserialized)}*: ");
+Console.WriteLine(JsonSerializer.Serialize(campeoesMundiaisDeserialized));
